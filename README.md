0b1dc3afe757fc2de8fd2520d3755f34c472fdc4
Kinuthia Ndung'u <kin.ndungu@gmail.com>
2022-11-29 07:08:55 +0000

    Add link to Chatlyzer

--- README.md
+++ README.md
@@ -1,9 +1,8 @@
 # Setting up
 
-  * `nvm install`
-  * `echo -e "\nexport PATH=/usr/local/share/nvm/versions/node/v18.12.0/bin:$PATH\n" >> /home/codespace/.bashrc`
-  * `source /home/codespace/.bashrc`
   * `npm i`
+  * `npm run dev`
+
 
 # Building
 
--- docs/_app/immutable/components/pages/_page.svelte-425e7bcb.js
+++
@@ -1,1 +0,0 @@
-import{S as r,i,s as c,k as h,q as m,l as d,m as p,r as _,h as l,b as f,G as u,B as n}from"../../chunks/index-8f0aa09d.js";function x(o){let e,s;return{c(){e=h("h1"),s=m("Hello")},l(t){e=d(t,"H1",{});var a=p(e);s=_(a,"Hello"),a.forEach(l)},m(t,a){f(t,e,a),u(e,s)},p:n,i:n,o:n,d(t){t&&l(e)}}}class g extends r{constructor(e){super(),i(this,e,null,x,c,{})}}export{g as default};
